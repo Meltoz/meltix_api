61e0cde9821d4a1347b4ac9a5b74b39318c806a2
Meltoz <meltoz@outlook.fr>
2025-09-21 13:49:35 +0200

    feat(mtix-54) remove endpoint and adding sorting

---
+++ Shared/SortOptionFactory.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Enums;
+
+namespace Shared
+{
+    public static class SortOptionFactory
+    {
+        public static SortOption<T> Create<T>(string? sortBy, string? direction) 
+            where T: struct, Enum
+        {
+            var sort = Enum.TryParse<T>(sortBy, true, out var parsedSort) ? parsedSort : default;
+            var dir = Enum.TryParse<SortDirection>(direction, true, out var parsedDirection) ? parsedDirection : SortDirection.Descending;
+
+            return new SortOption<T> { SortBy = sort, Direction = dir };
+        }
+    }
+}
