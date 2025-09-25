12759889d1774caeba834a8eb53afd77fc9b1e55
Meltoz <meltoz@outlook.fr>
2025-09-25 12:54:01 +0200

    feat(test): adding unit test on category

--- Meltix.UnitTests/Domain/CategoryTests.cs
+++ Meltix.UnitTests/Domain/CategoryTests.cs
@@ -5,7 +5,7 @@ namespace Meltix.UnitTests.Domain
     public class CategoryTests
     {
         [Fact]
-        public void Category_WithName_CreateComplete()
+        public void Category_ShouldCreate_WhenName()
         {
             // Arrange
             var categoryName = "Test";
@@ -18,5 +18,75 @@ namespace Meltix.UnitTests.Domain
             Assert.Equal(categoryName, category.Name);
         }
 
+        [Fact]
+        public void ChangeName_ShouldUpdateName_WhenName()
+        {
+            // Arrange
+            var categoryName = "Test";
+            var newCategoryName = "Test2";
+            var category = new Category(categoryName);
+
+            // Act
+            category.ChangeName(newCategoryName);
+
+            // Assert
+            Assert.NotNull(category);
+            Assert.Equal(newCategoryName, category.Name);
+        }
+
+        [Fact]
+        public void Category_ShouldThrow_WhenNoName()
+        {
+            // Arrange
+            string categoryName = null;
+            
+            // Act
+            var caughtException = Assert.ThrowsAny<Exception>(() => new Category(categoryName));
+
+            // Assert
+            Assert.IsType<ArgumentException>(caughtException);
+        }
+
+        [Fact]
+        public void Category_ShouldThrow_WhenWhiteSpace()
+        {
+            // Arrange
+            var categoryName = " ";
+
+            // Act
+            var caughtException = Assert.ThrowsAny<Exception>(() => new Category(categoryName));
+
+            // Assert
+            Assert.IsType<ArgumentException>(caughtException);
+        }
+
+        [Fact]
+        public void ChangeName_ShouldThrow_WhenSameName()
+        {
+            // Arrange
+            var categoryName = "Test";
+            var category = new Category(categoryName);
+
+            // Act
+            var caughtException = Assert.ThrowsAny<Exception>(() => category.ChangeName(categoryName));
+
+            // Assert
+            Assert.IsType<ArgumentException>(caughtException);
+        }
+
+        [Fact]
+        public void ChangeName_ShouldThrow_WhenNameInUppercase()
+        {
+            // Arrange
+            var categoryNameLower = "test";
+            var categoryNameUpper = categoryNameLower.ToUpper();
+            var category = new Category(categoryNameLower);
+
+            // Act
+            var caughtException = Assert.ThrowsAny<Exception>(() => category.ChangeName(categoryNameUpper));
+
+            // Assert
+            Assert.IsType<ArgumentException>(caughtException);
+        }
     }
 }
